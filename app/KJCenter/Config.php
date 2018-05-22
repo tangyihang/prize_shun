48a75cffb9a05d7c45b2e1dcbdef64860c003e8d
tangyihang <tangyihangchina@163.com>
2018-05-22 22:52:23 +0800

    数据库

--- app/KJCenter/Config.php
+++ app/KJCenter/Config.php
@@ -3,7 +3,7 @@
 //开奖后台配置信息
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PWD', '');
+define('DB_PWD', '1q2w3e4R!');
 define('DB_NAME', 'zhcwsystem');
 define('DB_PORT', 3306);
 define('DB_PREFIX', '');
